7f8da8ab574ed45259843f9332e06629e8e89d43
mconcs <mcon@cloudstaff.com>
2021-10-27 21:24:25 +0800

    Install lumen-generator and jwt-auth package

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -91,9 +91,11 @@ $app->routeMiddleware([
 |
 */
 
-$app->register(App\Providers\AppServiceProvider::class);
+// $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+$app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
+$app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
