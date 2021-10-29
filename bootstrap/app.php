01648b657c7a815a0a389a1922ffdcf5f0ebc22d
mcrupert onrejas <mconrejas@gmail.com>
2021-10-29 15:08:18 +0800

    Register service provider

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -95,6 +95,7 @@ $app->routeMiddleware([
 // $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+$app->register(App\Providers\RepositoryServiceProvider::class);
 $app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
 $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
 $app->register(\Anik\Form\FormRequestServiceProvider::class);
