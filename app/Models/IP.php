6372357affb9d977cf4a9b318bebcedab5e74e91
mcrupert onrejas <mconrejas@gmail.com>
2021-10-29 10:09:59 +0800

    Create IP model

---
+++ app/Models/IP.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
+
+class IP extends Model implements Auditable
+{
+    use HasFactory, \OwenIt\Auditing\Auditable;
+
+    protected $table = 'ips';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id', 'ip', 'label',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
